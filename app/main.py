3e86799a91aa6c6c73cafb5af4dc3ed60d400e41
Gregory Kimani <gregkymgreg@gmail.com>
2026-03-16 13:28:54 +0300

    Open Cv Code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('https://192.168.0.104:8080/video')
 
 widthImg = 720
 heightImg = 720
+
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    _, imgThresh = cv2.threshold(imgBlur, 127, 255, cv2.THRESH_BINARY_INV)
+
+    return imgThresh
+
 
 while cap.isOpened():
     succ, img = cap.read()
@@ -12,7 +21,13 @@ while cap.isOpened():
         print('Unable to open Video!!')
         break
 
-    cv2.imshow('Press ESC to exit', img)
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    img = cv2.resize(img, (widthImg, heightImg))
+
+    ###### Preprocessing ######
+    imgPreProcessed = preProcessing(img)
+
+    cv2.imshow('Press ESC to exit', imgPreProcessed)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
