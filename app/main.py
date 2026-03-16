94f0156c68cc8289e0a82b93bf106bdef84d4f72
Gregory Kimani <gregkymgreg@gmail.com>
2026-03-16 17:56:29 +0300

    Open Cv Code

--- app/main.py
+++ app/main.py
@@ -17,15 +17,15 @@ heightImg = 720
 
 def preProcessing(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 2)
     imgThresh = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                      cv2.THRESH_BINARY_INV, 11, 2)
-    kernel = np.ones((3, 3), np.uint8)
-    imgClosed = cv2.morphologyEx(imgThresh, cv2.MORPH_CLOSE, kernel)
-    # imgDilate = cv2.dilate(imgThresh, kernel, iterations=1)
-    # imgErode = cv2.erode(imgDilate, kernel=kernel, iterations=1)
+                                      cv2.THRESH_BINARY_INV, 25, 10)
+    kernel = np.ones((5, 5), np.uint8)
+    # imgClosed = cv2.morphologyEx(imgThresh, cv2.MORPH_CLOSE, kernel)
+    imgDilate = cv2.dilate(imgThresh, kernel, iterations=2)
+    imgErode = cv2.erode(imgDilate, kernel=kernel, iterations=1)
 
-    return imgClosed
+    return imgErode
 
 
 def getPredictions(img):
@@ -55,17 +55,31 @@ def findContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
 
-        if area > 700:
+        if area > 500:
             x, y, w, h = cv2.boundingRect(cnt)
 
             aspectRatio = float(w) / h
 
-            if 0.2 < aspectRatio < 1.0:
-                imgNumber = img[y:y+h, x:x+w]
+            if 0.1 < aspectRatio < 1.5:
+
+                # Ensure we don't crop outside the image boundaries
+                max_dim = max(w, h)
+                pad = 10
+
+                # Calculate center of the detected number
+                centerX, centerY = x + w // 2, y + h // 2
+
+                # Create a square bounding box
+                x1 = max(0, centerX - max_dim // 2 - pad)
+                y1 = max(0, centerY - max_dim // 2 - pad)
+                x2 = min(widthImg, centerX + max_dim // 2 + pad)
+                y2 = min(heightImg, centerY + max_dim // 2 + pad)
+
+                imgNumber = img[y1:y2, x1:x2]
 
                 number, confidence = getPredictions(imgNumber)
 
-                if confidence > 0.7:
+                if confidence > 0.20:
                     cv2.rectangle(imgContour, (x, y),
                                   (x+w, y+h), (0, 255, 0), 2)
                     cv2.putText(
