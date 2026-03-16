6bacea239d71a39eb881aba70e08e7c3a062e70d
Gregory Kimani <gregkymgreg@gmail.com>
2026-03-16 13:03:36 +0300

    Open Cv Code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# My Open Cv Code
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+widthImg = 720
+heightImg = 720
+
+while cap.isOpened():
+    succ, img = cap.read()
+
+    if not succ or img is None:
+        print('Unable to open Video!!')
+        break
+
+    cv2.imshow('Press ESC to exit', img)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
